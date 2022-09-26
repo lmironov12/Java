a86d645685a0e3c37707be7166d76698a550db84
lmironov12 <leonid.mironov12@gmail.com>
2022-09-26 18:57:23 +0300

    Types of data and variables

--- My first Java Project/src/com/company/Main.java
+++ My first Java Project/src/com/company/Main.java
@@ -2,6 +2,32 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int num = 5;
+        // byte [-128 128]
+        byte num2 = 127;
+        float num3 = 5.5f;
+        double num4 = 55.55d;
+        byte num5 = 10;
+        byte num6 = 15;
+        char sym = '$';
+        //boolean = true or false
+        boolean isTrue = true;
+        String FirstName = "Leonid";
+        String LastName = "Mironov";
+
+//        System.out.println(num2);
+//        System.out.println(num3);
+//        System.out.println(num4);
+//        System.out.println(num6);
+//        System.out.println(num5);
+//        System.out.println(sym);
+//        System.out.println(isTrue);
+//        System.out.print(FirstName +" ")
+//        System.out.print(LastName);
+
+        System.out.print(FirstName +" "+ LastName);
+
     }
+
 }
+
