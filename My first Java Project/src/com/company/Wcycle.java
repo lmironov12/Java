2257d2362df3cd983985a007088a3a8775b53695
lmironov12 <leonid.mironov12@gmail.com>
2022-10-10 18:45:20 +0300

    While cycle

---
+++ My first Java Project/src/com/company/Wcycle.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Wcycle {
+    public static void main(String[] args) {
+//        boolean isTrue = true;
+//        int i = 0;
+//        while (isTrue) {
+//            i++;
+//            System.out.println("Hello world");
+//            if (i > 5) {
+//                break;
+//        int i = 0;
+//                while (true){
+//                    i++;
+//                    System.out.println("Hello World");
+//                    if (i>5){
+//                    break;
+        int i = 0;
+        while (i<5){
+            i++;
+            System.out.println ("Hello World");
+            }
+        }
+    }
+
